04b1d233ffc28bd32d3d1eafd6555913b93e7c02
biobdeveloper <ibogomolov321@gmail.com>
2020-06-09 14:45:33 +0300

    Remove useless table
    Signed-off-by: biobdeveloper <ibogomolov321@gmail.com>

--- models/models.py
+++ models/models.py
@@ -6,21 +6,6 @@ from migrations import metadata
 Base = declarative_base()
 
 
-assets = sa.Table(
-    'assets', metadata,
-    sa.Column('id', sa.Integer, primary_key=True, index=True),
-    sa.Column("ticker", sa.String, unique=True),
-    sa.Column('name', sa.String, unique=True),
-    sa.Column('unified_cryptoasset_id', sa.Integer),
-    sa.Column("can_deposit", sa.Boolean),
-    sa.Column('can_withdraw', sa.Boolean),
-    sa.Column('min_withdraw', sa.Numeric),
-    sa.Column('max_withdraw', sa.Numeric),
-    sa.Column('maker_fee', sa.Numeric),
-    sa.Column("taker_fee", sa.Numeric),
-)
-
-
 class Asset(Base):
     __tablename__ = "assets"
 
