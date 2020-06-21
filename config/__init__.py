37cc605d8732d875fec7853e8c418fb12bd51387
biobdeveloper <ibogomolov321@gmail.com>
2020-06-21 13:19:09 +0300

    Change config logic, replace .yml files with .env
    Signed-off-by: biobdeveloper <ibogomolov321@gmail.com>

--- config/__init__.py
+++ config/__init__.py
@@ -1,13 +1,22 @@
-import yaml
-from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
 from config.const import *
 
 
-project_root_dir = Path(__file__).parent.parent
+load_dotenv()
 
-cfg = yaml.safe_load(open(f"{project_root_dir}/config/config.yml", 'r'))
+# Using in aiopg.sa
+pg_config = {
+    'host': os.getenv("DATABASE_HOST"),
+    'port': os.getenv("DATABASE_PORT"),
+    'user': os.getenv("DATABASE_USERNAME"),
+    'password': os.getenv("DATABASE_PASSWORD"),
+    'database': os.getenv("DATABASE_NAME")
+}
 
-pg_config = cfg["postgres"]
-sql_conn_url = f"postgres+psycopg2://{pg_config['user']}:{pg_config['password']}" \
+# Using in migrations
+sql_conn_url = f"postgresql+psycopg2://{pg_config['user']}:{pg_config['password']}" \
                f"@" \
                f"{pg_config['host']}:{pg_config['port']}/{pg_config['database']}"
